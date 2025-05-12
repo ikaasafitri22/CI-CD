935681514625a863810721efc4fca31c7c45cb77
ikasafitri <ikasafitri@students.amikom.ac.id>
2025-05-12 20:52:00 +0700

    Initial commit

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Hitung Durasi Nonton Film</title>
+</head>
+<body>
+    <h2>Hitung Durasi Nonton Film</h2>
+    <form method="POST">
+        Judul Film: <input type="text" name="judul"><br>
+        Jam Mulai: <input type="time" name="mulai"><br>
+        Jam Selesai: <input type="time" name="selesai"><br>
+        <button type="submit">Hitung</button>
+    </form>
+
+    <?php
+    if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+        $judul = $_POST['judul'];
+        $mulai = new DateTime($_POST['mulai']);
+        $selesai = new DateTime($_POST['selesai']);
+        $durasi = $mulai->diff($selesai);
+        echo "<p>Film: $judul</p>";
+        echo "<p>Durasi: " . $durasi->format('%h jam %i menit') . "</p>";
+    }
+    ?>
+</body>
+</html>
